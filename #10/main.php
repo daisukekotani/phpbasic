d31904b7cbb75c02be297724c8163fd9fdefb313
daisukekotani <green929.110mH@gmail.com>
2021-01-21 15:57:19 +0900

    #2〜10

---
+++ #10/main.php
@@ -0,0 +1,12 @@
+<?php
+
+// $name = 'taguchi';
+// $name = 'dotinstall';
+
+// define('NAME', 'taguchi');
+// define('NAME', 'dotinstall');
+// echo NAME . PHP_EOL;
+
+const NAME = 'taguchi';
+const NAME = 'dotinstall';
+echo NAME . PHP_EOL;
---
+++ #5/main.php
@@ -0,0 +1,11 @@
+<?php
+
+// $name = 'taguchi';
+$name = 'dotinstall';
+
+// echo 'Hello taguchi' . PHP_EOL;
+// echo 'Hi taguchi' . PHP_EOL;
+
+echo 'Hello ' . $name . PHP_EOL;
+// echo 'Hi ' . $name . PHP_EOL;
+echo 'Hi ' . $Name . PHP_EOL;
