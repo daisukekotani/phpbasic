fe6abf297747c069cee6d15c04c5fc0453bcf219
daisukekotani <green929.110mH@gmail.com>
2021-01-22 12:18:39 +0900

    #12-#20

---
+++ #12/main.php
@@ -0,0 +1,19 @@
+<?php
+
+$a = 'hello';
+$b = 10;
+$c = -1.3;
+$d = null;
+$e = true;
+
+// var_dump($a);
+// var_dump($b);
+// var_dump($c);
+// var_dump($d);
+// var_dump($e);
+
+$x = (float)5;
+$y = (string)3.2;
+
+var_dump($x);
+var_dump($y);
